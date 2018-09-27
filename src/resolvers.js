16779de9dd750ff99a4b6e9e108c620fc8cf2132
joaopluigi <joao.luigi@gmail.com>
2018-09-27 16:37:07 -0700

    add alertConfig query

--- src/resolvers.js
+++ src/resolvers.js
@@ -6,13 +6,13 @@ import { promisify } from 'util';
 const fetch = promisify(request);
 const url = 'https://dev3.agronomic.com:8105/api/v1';
 
-const getUsers = (bid, auth) => {
+const getUsers = auth => {
 
   // auth
   const authToken = new Buffer(`${auth.user}:${auth.password}`).toString('base64');
 
   const qs = {
-    'bid': bid,
+    'bid': auth.bid,
     'include_all_data': 1
   };
 
@@ -21,13 +21,68 @@ const getUsers = (bid, auth) => {
   };
 
   return fetch({ url: `${url}/Users`, qs: qs, headers: headers })
-    .then(res => JSON.parse(res.body))
+    .then(res => {
+
+      const users = JSON.parse(res.body);
+
+      if (!Array.isArray(users)) {
+        return {};
+      }
+
+      return users.map(user => ({
+        'id': user.user_id,
+        'username': user.username,
+        'name': user.name,
+        'email': user.email,
+        'mobile_phone': user.mobile,
+        'last_login': user.last_login
+      }));
+    })
+    .catch(err => err);
+};
+
+const getUserAlerts = (userId, auth) => {
+
+  // auth
+  const authToken = new Buffer(`${auth.user}:${auth.password}`).toString('base64');
+
+  const qs = {
+    'bid': auth.bid,
+    'include_all_data': 1
+  };
+
+  const headers = {
+    'Authorization': `Basic ${authToken}`
+  };
+
+  return fetch({ url: `${url}/Users`, qs: qs, headers: headers })
+    .then(res => {
+
+      const users = JSON.parse(res.body);
+
+      if (!Array.isArray(users)) {
+        return {};
+      }
+
+      const user = users.filter(user => user.user_id === userId)[0];
+
+      return {
+        alerts_enabled: user.alerts_enabled,
+        email_alert_days: user.email_alert_days,
+        email_alert_point_verbosity: user.email_alert_point_verbosity,
+        email_alert_zone_verbosity: user.email_alert_zone_verbosity,
+        sms_alert_days: user.sms_alert_days,
+        alert_start: user.alert_start,
+        alert_end: user.alert_end
+      };
+    })
     .catch(err => err);
 };
 
 const resolvers = {
   Query: {
-    users: (root, args) => getUsers(args.bid, args.auth)
+    users: (root, args) => getUsers(args.auth),
+    alertConfig: (root, args) => getUserAlerts(args.userId, args.auth)
   }
 };
 
--- src/schema.js
+++ src/schema.js
@@ -2,34 +2,31 @@
 
 const schema = `
 type Query {
-    users(bid: Int!, auth: Auth!): [Users!]!
+    users(auth: Auth!): [Users!]!
+    alertConfig(userId: Int!, auth: Auth!): AlertConfig!
 }
 
 type Users {
-
-    user_id: Int!
+    id: Int!
     username: String!
     name: String!
     email: String!
-    mobile: String!
-    
-    user_type_id: Int!
-    grower_account_id: Int!
-    field_activation_right_id: Int!
-    first: String!
-    last: String!
-    alert_start: String
-    alert_end: String
+    mobile_phone: String!
     last_login: String
-    sms_alert_days: Int!
+}
+
+type AlertConfig {
     alerts_enabled: Int!
     email_alert_days: Int!
-    email_alert_zone_verbosity: Int!
     email_alert_point_verbosity: Int!
-    
+    email_alert_zone_verbosity: Int!
+    sms_alert_days: Int!
+    alert_start: String
+    alert_end: String
 }
 
 input Auth {
+    bid: Int!
     user: String!
     password: String!
 }
